56801e4113b9fbf502cd70cfd19f172053271bd8
awul <ilhamnopi@icloud.com>
2025-03-12 14:08:35 +0700

    initialize project

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Application, NextFunction, Request, Response } from 'express'
+
+const app: Application = express()
+const port: number = 4000
+
+app.use('/health', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({ status: '200' })
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
