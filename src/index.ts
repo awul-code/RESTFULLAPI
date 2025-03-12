8d99273bc30b64c20e329a2d9d1c1258ebdf4a54
awul <ilhamnopi@icloud.com>
2025-03-13 02:48:02 +0700

    feat: add body-parser and cors middleware
    - imported bodyParser and cors module.
    - Added midleware to parse URL-encoded and JSON body requests.
    - Implemented CORS handling middleware to set  appropriate headers for cors-origin requests.
    - Ensured compatibility with existing routes by incorporating the middleware into ProductRouter

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,24 @@ import express from 'express'
 import type { Application } from 'express'
 import { routes } from './routes/index'
 import { logger } from './utils/logger'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 
 const app: Application = express()
 const port: number = 4000
+
+// parse body request
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// cors access handler
+app.use(cors())
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', '*')
+    res.setHeader('Access-Control-Allow-Headers', '*')
+    next()
+})
 
 routes(app)
 
