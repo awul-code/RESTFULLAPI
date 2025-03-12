abeebb6929de8db7842d75b08e890064faa84ca2
awul <ilhamnopi@icloud.com>
2025-03-13 02:40:52 +0700

    feat: Add POST route to ProductionRoute
    - Add a new POST route to ProductionRoute that will allow the user to add a new production to the database.

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -7,3 +7,8 @@ ProductRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
     logger.info('Product list success')
     res.status(200).send({ status: true, statusCode: 200, data: [{ name: 'Sepatu Sport', price: 500000 }] })
 })
+
+ProductRouter.post('/', (req: Request, res: Response, next: NextFunction) => {
+    logger.info('Success create new product')
+    res.status(200).send({ status: true, statusCode: 200, data: req.body })
+})
