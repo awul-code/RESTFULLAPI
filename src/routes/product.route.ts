55f02a75960ec8510cd24b6c41b22694d7a700b9
awul <ilhamnopi@icloud.com>
2025-03-13 03:36:21 +0700

    feat: add validation for product creation endpoint
    - Implement product creation validation using createProductValidation function.
    - Add error handling for validation failures, returning a 422 status code and with an error message.
    - Logged validation error for better debugging and tracking.
    - Successfully created new product, logging a success message and returning  a 200 status code with created product data.

---
+++ src/routes/product.route.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response, Router } from 'express'
+import { logger } from '../utils/logger'
+import { createProductValidation } from '../validation/product.validation'
+
+export const ProductRouter: Router = Router()
+
+ProductRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
+    logger.info('Product list success')
+    res.status(200).send({ status: true, statusCode: 200, data: [{ name: 'Sepatu Sport', price: 500000 }] })
+    return
+})
+
+ProductRouter.post('/', (req: Request, res: Response, next: NextFunction) => {
+    const { error, value } = createProductValidation(req.body)
+    if (error) {
+        logger.error('Validation ERR product-create: ', error.details[0].message)
+        res.status(422).send({ status: false, statusCode: 422, message: error.details[0].message, data: {} })
+        return
+    }
+    logger.info('Success create new product')
+    res.status(200).send({ status: true, statusCode: 200, message: 'Add product success', data: value })
+    return
+})
