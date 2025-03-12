d4570c65f12ce8db6b7c5f9055fb232867c99041
awul <ilhamnopi@icloud.com>
2025-03-12 16:28:30 +0700

    Set up ESLint, Prettier, Husky

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app: Application = express()
 const port: number = 4000
 
 app.use('/health', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({ status: '200' })
+    res.status(200).send({ status: '200', data: 'hello world' })
 })
 
 app.listen(port, () => {
