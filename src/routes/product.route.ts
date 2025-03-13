5d6672cb2307446557b37a7ca40f1422d920968b
awul <ilhamnopi@icloud.com>
2025-03-13 11:33:41 +0700

    feat: integrated product controllers into product.route.ts
    - Replace inline logic with dedicated controller function.
    - Call `getProduct` for both default and dynamic GET routes (using "name" as a parameter).
    - Delegate product creation to the `createProduct` controller.

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,23 +1,9 @@
-import { NextFunction, Request, Response, Router } from 'express'
-import { logger } from '../utils/logger'
-import { createProductValidation } from '../validations/product.validation'
+import { Router } from 'express'
+
+import { createProduct, getProduct } from '../controllers/product.controller'
 
 export const ProductRouter: Router = Router()
 
-ProductRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
-    logger.info('Product list success')
-    res.status(200).send({ status: true, statusCode: 200, data: [{ name: 'Sepatu Sport', price: 500000 }] })
-    return
-})
-
-ProductRouter.post('/', (req: Request, res: Response, next: NextFunction) => {
-    const { error, value } = createProductValidation(req.body)
-    if (error) {
-        logger.error('Validation ERR product-create: ', error.details[0].message)
-        res.status(422).send({ status: false, statusCode: 422, message: error.details[0].message, data: {} })
-        return
-    }
-    logger.info('Success create new product')
-    res.status(200).send({ status: true, statusCode: 200, message: 'Add product success', data: value })
-    return
-})
+ProductRouter.get('/', getProduct)
+ProductRouter.get('/:name', getProduct)
+ProductRouter.post('/', createProduct)
