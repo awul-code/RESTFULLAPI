5aaa1015059b442f3d3cd1038f8354a459246db3
awul <ilhamnopi@icloud.com>
2025-03-20 08:39:30 +0700

    feat: add and update product database service function
    - Created `deleteProductById` to remove a product using its ID
    - Refactored `updateProductById` to improve code readability and debugging.

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -23,5 +23,11 @@ export const getProductById = async (id: string) => {
 }
 
 export const updateProductById = async (id: string, payload: ProductType) => {
-    return await productModel.findOneAndUpdate({ product_id: id }, { $set: payload })
+    const result = await productModel.findOneAndUpdate({ product_id: id }, { $set: payload })
+    return result
+}
+
+export const deleteProductById = async (id: string) => {
+    const result = await productModel.findOneAndDelete({ product_id: id })
+    return result
 }
