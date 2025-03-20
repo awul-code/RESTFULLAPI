349504479c971af052c24a0ee147f1222b98c1d3
awul <ilhamnopi@icloud.com>
2025-03-20 08:46:14 +0700

    feat: enhance updateProductById and ad deleteProductById function with error handling
    - refactor updateProductById function to handle product update errors
    - log validation error with status code 404 and failure message
    - Developed deleteProduct to remove a product by ID
    - Extracts the ID from request parameters.
      - Attempts deletion using deleteProductById.
      - Sends a 200 response on successful deletion with a success log.
      - Sends a 404 response with a log when the product is not found.
      - Catches and logs any errors, returning a 500 response for internal server errors.

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,7 +1,13 @@
 import { Response, Request } from 'express'
 import { createProductValidation, updateProductValidation } from '../validations/product.validation'
 import { logger } from '../utils/logger'
-import { addProductToDB, getProductById, getProductFromDB, updateProductById } from '../services/product.service'
+import {
+    addProductToDB,
+    deleteProductById,
+    getProductById,
+    getProductFromDB,
+    updateProductById
+} from '../services/product.service'
 import { v4 as uuidv4 } from 'uuid'
 
 export const getProduct = async (req: Request, res: Response) => {
@@ -46,7 +52,7 @@ export const createProduct = async (req: Request, res: Response) => {
     try {
         await addProductToDB(value)
         logger.info('Success create new product')
-        res.status(200).send({ status: true, statusCode: 201, message: 'Add new product success' })
+        res.status(201).send({ status: true, statusCode: 201, message: 'Add new product success' })
         return
     } catch (error) {
         logger.error('Validation ERR product-create: ', error)
@@ -68,12 +74,42 @@ export const updateProduct = async (req: Request, res: Response) => {
     }
 
     try {
-        await updateProductById(id, value)
-        logger.info('Success update  product')
-        res.status(201).send({ status: true, statusCode: 201, message: 'Update product success' })
-        return
+        const result = await updateProductById(id, value)
+
+        if (result) {
+            logger.info('Success update  product')
+            res.status(201).send({ status: true, statusCode: 201, message: 'Update product success' })
+            return
+        } else {
+            logger.info('Data not found')
+            res.status(404).send({ status: false, statusCode: 404, message: 'Data not found' })
+            return
+        }
     } catch (error) {
         logger.error('Validation ERR product-update: ', error)
+        res.status(422).send({ status: false, statusCode: 422, message: error })
+        return
+    }
+}
+
+export const deleteProduct = async (req: Request, res: Response) => {
+    const {
+        params: { id }
+    } = req
+    try {
+        const result = await deleteProductById(id)
+
+        if (result) {
+            logger.info('Success delete product')
+            res.status(200).send({ status: true, statusCode: 200, message: 'Delete product success' })
+            return
+        } else {
+            logger.info('Data not found')
+            res.status(404).send({ status: false, statusCode: 404, message: 'Data not found' })
+            return
+        }
+    } catch (error) {
+        logger.error('Validation ERR product-delete: ', error)
         res.status(422).send({ status: false, statusCode: 422, message: error })
         return
     }
